824b9e1aa094e76318251caeb875053678820740
Daniel Velinov <46249988+daniel-velinov@users.noreply.github.com>
2023-12-05 10:33:32 +0200

    Update linux-performance-check.sh

--- linux-performance-check.sh
+++ linux-performance-check.sh
@@ -2,16 +2,11 @@
 
 logfile="/var/lib/zabbix/performance-stats-$(date +'%d-%m-%Y').log"
 
-{
-echo -e "\nCPU Statistics\n"
-top -n 1 -b -o %CPU | head -30
-
-echo -e "\n\nMemory Statistics\n"
-top -n 1 -b -o %MEM | head -30
-
-echo -e "\n\nIO Statistics\n"
-iotop -o -b -n 1 -q
-
-echo -e "\n\nTraffic Statistics\n"
-iftop -nNPt -i ens192 -s 5
-} > "$logfile"
+echo -e "\nCPU Statistics\n" >> $logfile
+top -n 1 -b -o %CPU | head -30 >> $logfile
+echo -e "\n\nMemory Statistics\n" >> $logfile
+top -n 1 -b -o %MEM | head -30 >> $logfile
+echo -e "\n\nIO Statistics\n" >> $logfile
+iotop -o -b -n 1 -q >> $logfile
+echo -e "\n\nTraffic Statistics\n" >> $logfile
+iftop -nNPt -i ens192 -s 5 >> $logfile
