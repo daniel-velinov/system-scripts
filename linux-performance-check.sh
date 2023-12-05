87efacef44627fe720f47a3b730a62c49252e974
Daniel Velinov <46249988+daniel-velinov@users.noreply.github.com>
2023-12-05 10:18:30 +0200

    Update linux-performance-check.sh

--- linux-performance-check.sh
+++ linux-performance-check.sh
@@ -1,10 +1,10 @@
-#! /bin/bash
-logfile = "/var/lib/zabbix/performance-stats-$(date +'%d-%m-%Y')"
-echo -e "\nCPU Statistics\n" >> $logfile
-top -n 1 -b -o %CPU | head -30 >> $logfile
-echo -e "\n\nMemory Statistics\n" >> $logfile
-top -n 1 -b -o %MEM | head -30 >> $logfile
-echo -e "\n\nIO Statistics\n" >> $logfile
-iotop -o -b -n 1 -q >> $logfile
-echo -e "\n\nTraffic Statistics\n" >> $logfile
-iftop -nNPt -i ens192 -s 5 >> $logfile
+#!/bin/bash
+logfile="/var/lib/zabbix/performance-stats-$(date +'%d-%m-%Y')"
+echo -e "\nCPU Statistics\n" >> "$logfile"
+top -n 1 -b -o %CPU | head -30 >> "$logfile"
+echo -e "\n\nMemory Statistics\n" >> "$logfile"
+top -n 1 -b -o %MEM | head -30 >> "$logfile"
+echo -e "\n\nIO Statistics\n" >> "$logfile"
+iotop -o -b -n 1 -q >> "$logfile"
+echo -e "\n\nTraffic Statistics\n" >> "$logfile"
+iftop -nNPt -i ens192 -s 5 >> "$logfile"
